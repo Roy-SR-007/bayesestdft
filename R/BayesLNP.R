92d90a6efeb0e6f9c9710f2da4b4e9190da35e0b
Somjit Roy <sroy_123@tamu.edu>
2025-01-07 22:11:14 +0530

    1

--- R/BayesLNP.R
+++ R/BayesLNP.R
@@ -1,3 +1,36 @@
+#'
+#' @title Estimating the Student-t degrees of freedom (dof) with a Log-normal Prior over the dof
+#'
+#' @description `BayesLNP` samples from the posterior distribution of the degrees of freedom (dof) with Log-normal prior endowed upon the dof, using an Elliptical Slice Sampler (ESS).
+#'
+#' @param y an N-dimensional vector of continuous observations supported on the real-line
+#' @param ini.nu the initial posterior sample value of the degrees of freedom (default is 1)
+#' @param S the number of posterior samples (default is 1000)
+#' @param mu mean of the Log-normal prior density (default is 1)
+#' @param sigma.sq variance of the Log-normal prior density (default is 1)
+#'
+#' @return A vector of posterior sample estimates
+#' \item{res}{an S-dimensional vector with the posterior samples}
+#'
+#' @export
+#'
+#' @examples
+#'
+#' # data from Student-t distribution with dof = 0.1
+#' y = rt(n = 100, df = 0.1)
+#'
+#' # running the Elliptical Slice Sampler (ESS) with default settings
+#' nu = BayesLNP(y)
+#' # reporting the posterior mean estimate of the dof
+#' mean(nu)
+#'
+#' @references
+#' Lee, S. Y. (2022). "The Use of a Log-Normal Prior for the Student t-Distribution",
+#' \emph{Axioms}, <https://doi.org/10.3390/axioms11090462>
+#'
+#' Murray, I., Prescott Adams, R., MacKay, D. J. (2010). "Elliptical slice sampling",
+#' \emph{Proceedings of the Thirteenth International Conference on Artificial Intelligence and Statistics}, <https://proceedings.mlr.press/v9/murray10a>
+
 BayesLNP = function(y, ini.nu = 1 , S = 1000, mu = 1, sigma.sq = 1){
 
   # Sample size
